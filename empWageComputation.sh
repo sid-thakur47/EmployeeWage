7c7a64c2f883b3f3b52ba459670c420dbd812340
sid-thakur47 <sidthakur258@gmail.com>
2020-03-28 18:43:29 +0530

    ADDED:empWageComputation To calculate wages for month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,18 +1,21 @@
 #!/bin/bash -x
-
+numWorkingDays=20
 empRatePerHour=20
-empRandomCheck=$(($RANDOM%3))
-
-case $empRandomCheck in
-			1)
-				empHours=4
-				;;
-			2)
-				empHours=8
-				;;
-			*)
-				empHours=0
-				;;
+for (( day=1; day<=$numWorkingDays; day++ )) 
+ do
+	empRandomCheck=$(($RANDOM%3))
+		case $empRandomCheck in
+				1)
+					empHours=4
+					;;
+				2)
+					empHours=8
+					;;
+				*)
+					empHours=0
+					;;
 esac
-sal=$(($empRatePerHour * $empHours)) 
-echo $sal
+sal=$(($empRatePerHour * $empHours))
+totalSalary=$(($totalSalary + $sal))
+done
+echo "Total Salary" $totalSalary
