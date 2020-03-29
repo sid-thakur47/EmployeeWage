aac9d3b01c87aa22549dbc96162f5801fd026598
sid-thakur47 <sidthakur258@gmail.com>
2020-03-29 13:40:07 +0530

    ADDED:empWageComputation To store Wages into array

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+declare -a empDailyWage
 NUM_WORKING_DAYS=20
 EMP_RATE_PER_HOUR=20
 MAX_HR_IN_MONTH=4
@@ -6,7 +7,7 @@ MAX_HR_IN_MONTH=4
 totalEmpHr=0
 totalWorkingDays=0
 function getWorkingHours() {
-	case $1 in
+	case $rand in
 		1)
 			workHours=4
 			;;
@@ -19,14 +20,22 @@ function getWorkingHours() {
 	esac
 		echo $workHours
 }
+
+function getEmpDailyWage() {
+wagePerDay=$(($workHours * $EMP_RATE_PER_HOUR))
+echo $wagePerDay
+}
+
 while [[ $totalWorkHours -lt $MAX_HR_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
  do
 	((totalWorkingDays++))
 	rand=$(($RANDOM%3))
 	workHours="$( getWorkingHours $rand  )"
 	totalWorkingHours=$(($totalWorkingHours + $workHours))
+	#empDailyWage[totalWorkingDays]="(( getEmpDailyWage $workHours))"
+	empDailyWage[$totalWorkingDays]="$(getEmpDailyWage)"
 done
 totalSalary=$(($totalWorkingHours * $EMP_RATE_PER_HOUR))
 echo $totalWorkingHours $totalSalary
-
+echo "Daily Wage:"${empDailyWage[*]}
 
