81c91d820b3d5969854574326d41d64bc708f3c3
sid-thakur47 <sidthakur258@gmail.com>
2020-03-29 11:14:12 +0530

    REFACTORED:empWageComputation Created function to get work hrs.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,26 +1,32 @@
 #!/bin/bash -x
 NUM_WORKING_DAYS=20
 EMP_RATE_PER_HOUR=20
-MAX_HR_IN_MONTH=10
+MAX_HR_IN_MONTH=4
 
 totalEmpHr=0
 totalWorkingDays=0
-while [[ $totalEmpHr -lt $MAX_HR_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+function getWorkingHours() {
+	case $1 in
+		1)
+			workHours=4
+			;;
+		2)
+			workHours=8
+			;;
+		*)
+			workHours=0
+			;;
+	esac
+		echo $workHours
+}
+while [[ $totalWorkHours -lt $MAX_HR_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
  do
 	((totalWorkingDays++))
-	 empRandomCheck=$(($RANDOM%3))
-		case $empRandomCheck in
-				1)
-					empHours=4
-					;;
-				2)
-					empHours=8
-					;;
-				*)
-					empHours=0
-					;;
-		esac
-totalEmpHr=$(($totalEmpHr+$empHours))
+	rand=$(($RANDOM%3))
+	workHours="$( getWorkingHours $rand  )"
+	totalWorkingHours=$(($totalWorkingHours + $workHours))
 done
-totalSalary=$(($totalEmpHr * $EMP_RATE_PER_HOUR))
-echo $totalEmpHr $totalSalary
+totalSalary=$(($totalWorkingHours * $EMP_RATE_PER_HOUR))
+echo $totalWorkingHours $totalSalary
+
+
